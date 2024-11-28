316f9aa0140ffb0d760b6f0adad6b450bf44ee0d
DamThiLanAnh <damthilananh8@hmail.com>
2024-11-28 15:09:03 +0700

    init docker

--- BE/pkg/db/schema/user_base.sql
+++ BE/pkg/db/schema/user_base.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS user_base (
-    user_id         SERIAL PRIMARY KEY, --User ID
-    user_account    VARCHAR(255) NOT NULL,     --User account (user to verify identity)
-    user_password   VARCHAR(255) NOT NULL,    --User password
-    user_salt       VARCHAR(255) NOT NULL,        --User salt for password encryption
+    user_id         SERIAL PRIMARY KEY,         --User ID
+    user_account    VARCHAR(255) NOT NULL,      --User account (user to verify identity)
+    user_password   VARCHAR(255) NOT NULL,      --User password
+    user_salt       VARCHAR(255) NOT NULL,      --User salt for password encryption
 -- is Two Factor Enabled
-    user_login_time TIMESTAMP NULL,         --last login time
-    user_logout_time TIMESTAMP NULL,        --last logout time
+    user_login_time TIMESTAMP NULL,              --last login time
+    user_logout_time TIMESTAMP NULL,             --last logout time
     user_login_ip      VARCHAR(45) NULL,         --Login ip address
 
     user_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --record creation time
--- BE/pkg/db/schema/user_verify.sql
+++ BE/pkg/db/schema/user_verify.sql
@@ -4,4 +4,5 @@ CREATE IF NOT EXISTS user_two_factor (
        two_factor_auth_type TEXT CHECK (two_factor_auth_type IN ('SMS', 'EMAIL', 'APP')) NOT NULL
 -- ensure two_factor_id not negative
 CONSTRAINT check_user_id_positive CHECK (user_id > 0)
+
 )
