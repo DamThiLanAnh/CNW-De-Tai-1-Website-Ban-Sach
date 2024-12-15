2145d5fd7bf7531737494e67f6924875f107430d
DamThiLanAnh <damthilananh8@hmail.com>
2024-12-15 19:41:23 +0700

    recommened menu

--- FE/src/pages/home/Home.jsx
+++ FE/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 // import React from 'react'
 import Banner from './Banner'
 import TopSales from './TopSales'
+import Recommened from './Recommened'
 const Home = () => {
     return (
         <>
             <Banner />
             <TopSales />
+            <Recommened />
         </>
     )
 }
