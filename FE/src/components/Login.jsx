6d1d2ded9643a3a2169f0835cba4cead6d2309f1
DamThiLanAnh <damthilananh8@hmail.com>
2024-12-19 16:14:49 +0700

    fix bug

--- FE/src/components/Login.jsx
+++ FE/src/components/Login.jsx
@@ -1,4 +1,3 @@
-import Link from 'react-router-dom'
 import { FaGoogle } from "react-icons/fa";
 
 const Login = () => {
--- FE/src/routers/router.jsx
+++ FE/src/routers/router.jsx
@@ -33,6 +33,7 @@ const router = createBrowserRouter([
                 path: '/login',
                 element: <Login />
             },
+
             {
                 path: '/register',
                 element: <Register />
