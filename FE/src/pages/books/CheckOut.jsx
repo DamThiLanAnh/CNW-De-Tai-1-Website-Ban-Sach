7109a3191fc0ef52d1d0e53ab1141faa7b3c01ed
DamThiLanAnh <damthilananh8@hmail.com>
2025-01-03 17:59:21 +0700

    done checkout

--- FE/src/pages/books/CheckOut.jsx
+++ FE/src/pages/books/CheckOut.jsx
@@ -1,15 +1,10 @@
 import { Link } from "react-router-dom"
 import { useSelector } from "react-redux"
-<<<<<<< HEAD
 import { useForm } from "react-hook-form"
-import { useState } from "react"
-=======
-import { useForm } from "react-hook-form";
 import { useState } from "react";
->>>>>>> d133c2af20f0e37c136555eee9331e28f4de141a
 
 const CheckOut = () => {
-    const cartItems = useSelector((state) => state.cart.cartItems)
+    const cartItems = useSelector(state => state.cart.cartItems)
     const totalPrice = cartItems.reduce((acc, item) => acc + item.newPrice, 0).toFixed(2)
     const currentUser = true;
     const {
@@ -18,9 +13,10 @@ const CheckOut = () => {
         watch,
         formState: { errors },
     } = useForm();
+
     const [isChecked, setIsChecked] = useState(false)
     const onSubmit = (data) => {
-        console.log(data)
+
         const newOrder = {
             name: data.name,
             email: currentUser.email,
@@ -28,7 +24,7 @@ const CheckOut = () => {
                 city: data.city,
                 country: data.country,
                 state: data.state,
-                street: data.street
+                zipcode: data.zipcode
             },
             phone: data.phone,
             productIds: cartItems.map(item => item?._id),
@@ -43,7 +39,7 @@ const CheckOut = () => {
                 <div className="container max-w-screen-lg mx-auto">
                     <div>
                         <div>
-                            <h2 className="font-semibold text-xl text-gray-600 mb-2">Cash On Delevary</h2>
+                            <h2 className="font-semibold text-xl text-gray-600 mb-2">Cash On Delivery</h2>
                             <p className="text-gray-500 mb-2">Total Price: ${totalPrice}</p>
                             <p className="text-gray-500 mb-6">Items: {cartItems.length > 0 ? cartItems.length : 0}</p>
                         </div>
@@ -70,7 +66,7 @@ const CheckOut = () => {
                                             <div className="md:col-span-5">
                                                 <label html="email">Email Address</label>
                                                 <input
-                                                    {...register("email", { required: true })}
+                                                    // {...register("email", { required: true })}
                                                     type="text" name="email" id="email" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                                                     disabled
                                                     defaultValue={currentUser?.email}
@@ -80,11 +76,7 @@ const CheckOut = () => {
                                                 <label html="phone">Phone Number</label>
                                                 <input
                                                     {...register("phone", { required: true })}
-<<<<<<< HEAD
                                                     type="number" name="phone" id="phone" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="your phone number" />
-=======
-                                                    type="number" name="phone" id="phone" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="+123 456 7890" />
->>>>>>> d133c2af20f0e37c136555eee9331e28f4de141a
                                             </div>
 
                                             <div className="md:col-span-3">
@@ -124,7 +116,10 @@ const CheckOut = () => {
                                                 <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                                                     <input
                                                         {...register("state", { required: true })}
-                                                        name="state" id="state" placeholder="State" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" />
+                                                        name="state"
+                                                        id="state"
+                                                        placeholder="State"
+                                                        className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" />
                                                     <button className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
                                                         <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                                             <line x1="18" y1="6" x2="6" y2="18"></line>
@@ -147,8 +142,12 @@ const CheckOut = () => {
                                             <div className="md:col-span-5 mt-3">
                                                 <div className="inline-flex items-center">
                                                     <input
-                                                        type="checkbox" name="billing_same" id="billing_same" className="form-checkbox" />
-                                                    <label htmlFor="billing_same" className="ml-2 ">I am aggree to the
+                                                        type="checkbox"
+                                                        name="billing_same"
+                                                        id="billing_same"
+                                                        className="form-checkbox"
+                                                        onChange={(e) => setIsChecked(e.target.checked)} />
+                                                    <label htmlFor="billing_same" className="ml-2 ">I am agree to the
                                                         <Link className='underline underline-offset-2 text-blue-600'>
                                                             Terms & Conditions
                                                         </Link>
